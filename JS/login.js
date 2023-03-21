aecc7ad52032441677ee509c161a73dc60a01381
GriffinWilchuk <gwilchuk@student.ubc.ca>
2023-03-21 14:36:47 -0700

    log in validation

--- JS/login.js
+++ JS/login.js
@@ -1,13 +1,18 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', (event) => {
+});
     //the event occurred
-  
+    
 
 function validateForm() {
-    let x = document.forms["loginForm"]["username"].value;
-    if (x == "") {
-      alert("Name must be filled out");
+    let username = document.forms["loginForm"]["username"].value;
+    if (username == "") {
+      alert("Please Enter your Username");
+      return false;
+    }
+
+    let psw = document.forms["loginForm"]["psw"].value;
+    if (psw == "") {
+      alert("Please Enter your Password");
       return false;
     }
   }
-
-})
